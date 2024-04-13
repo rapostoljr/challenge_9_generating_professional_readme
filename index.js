5ea8d75600258a0dc5327eb12f20fd4fa401d562
unknown <romuloapostol@gmail.com>
2024-04-12 19:37:25 -0700

    added packages needed for project, created questions to ask to create README generator

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter title for project.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project, your reason for creating it.",
+    },
+    {
+        type: "input",
+        name: "table_of_contents",
+        message: "Create a table of content for your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe how to install your project."
+    },
+    {
+        type: "input",
+        name: "how_to_use",
+        message: "Describe how to use your project."
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Add a description of how to become a contributor in this project."
+    },
+    {
+        type: "input",
+        name: "test_instructions",
+        message: "How to test your project."
+    },
+    {
+        type: "checkbox",
+        name: "licenses",
+        message: "Please select the license you want to associate with this project.",
+        choices: ["MIT", "APACHE2.0", "BSD-3", "MPL-2.0", "GPLv3", "none"]
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address."
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
